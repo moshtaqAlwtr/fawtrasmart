4adaeac26216d1a7fc9d6593d0f623f63b7608d2
mon3em <62883698+abdu0560@users.noreply.github.com>
2025-04-12 22:23:06 +0300

    Update Neighborhood.php

--- app/Models/Neighborhood.php
+++ app/Models/Neighborhood.php
@@ -10,5 +10,8 @@ class Neighborhood extends Model
 {
     return $this->belongsTo(Region_groub::class, 'region_id');
 }
-
+public function client()
+{
+    return $this->belongsTo(Client::class, 'client_id');
+}
 }
